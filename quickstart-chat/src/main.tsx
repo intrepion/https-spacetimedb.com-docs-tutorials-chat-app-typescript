869ac8308f6fe7ba13002c1e3f7936b687a25ec1
Oliver Forral <intrepion@gmail.com>
2026-06-10 20:24:49 -0600

    Generate your module types

--- quickstart-chat/src/main.tsx
+++ quickstart-chat/src/main.tsx
@@ -1,7 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Identity } from 'spacetimedb';
+import { SpacetimeDBProvider } from 'spacetimedb/react';
+import { DbConnection, type ErrorContext } from './module_bindings/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
